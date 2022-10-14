66fc3004e4e24be2f156ef1771b7f5d14bc2964f
NathanStanding <standingtall1000@gmail.com>
2022-10-13 22:17:49 -0600

    select random campsite

--- src/features/campsites/campstiesSlice.js
+++ src/features/campsites/campstiesSlice.js
@@ -3,3 +3,7 @@ import {CAMPSITES} from "../../app/shared/CAMPSITES.js";
 export const selectAllCampsites = () => {
     return CAMPSITES;
 };
+
+export const selectRandomCampsite = () => {
+    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+};
